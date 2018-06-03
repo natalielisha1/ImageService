34debbd44ee694a1b270cbe5c2b173f60feb8044
Ofek Segal <ofek286@gmail.com>
2018-06-04 01:37:12 +0300

    Added Student DB (with some garbage that will be cleaned later on)

---
+++ ImageServiceWEB/Controllers/StudentDetailsController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ImageServiceWEB.Models;
+
+namespace ImageServiceWEB.Controllers
+{
+    public class StudentDetailsController : Controller
+    {
+        private StudentDetailsDbContext db = new StudentDetailsDbContext();
+
+        // GET: StudentDetails
+        public ActionResult Index()
+        {
+            //db.Students.Add(new StudentDetails
+            //{
+            //    IsrID = 315638288,
+            //    FirstName = "Ofek",
+            //    LastName = "Segal"
+            //});
+            //db.Students.Add(new StudentDetails
+            //{
+            //    IsrID = 209475458,
+            //    FirstName = "Natalie",
+            //    LastName = "Elisha"
+            //});
+            //db.SaveChanges();
+            return View(db.Students.ToList());
+        }
+
+        // GET: StudentDetails/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            StudentDetails studentDetails = db.Students.Find(id);
+            if (studentDetails == null)
+            {
+                return HttpNotFound();
+            }
+            return View(studentDetails);
+        }
+
+        // GET: StudentDetails/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: StudentDetails/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,FirstName,LastName")] StudentDetails studentDetails)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Students.Add(studentDetails);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(studentDetails);
+        }
+
+        // GET: StudentDetails/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            StudentDetails studentDetails = db.Students.Find(id);
+            if (studentDetails == null)
+            {
+                return HttpNotFound();
+            }
+            return View(studentDetails);
+        }
+
+        // POST: StudentDetails/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,FirstName,LastName")] StudentDetails studentDetails)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(studentDetails).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(studentDetails);
+        }
+
+        // GET: StudentDetails/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            StudentDetails studentDetails = db.Students.Find(id);
+            if (studentDetails == null)
+            {
+                return HttpNotFound();
+            }
+            return View(studentDetails);
+        }
+
+        // POST: StudentDetails/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            StudentDetails studentDetails = db.Students.Find(id);
+            db.Students.Remove(studentDetails);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ImageServiceWEB/Models/StudentDetails.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace ImageServiceWEB.Models
+{
+    public class StudentDetails
+    {
+        //Key
+        public int Id { get; set; }
+        //Real ID
+        public int IsrID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    public class StudentDetailsDbContext : DbContext
+    {
+        public DbSet<StudentDetails> Students { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            Database.SetInitializer<StudentDetailsDbContext>(new CreateDatabaseIfNotExists<StudentDetailsDbContext>());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
